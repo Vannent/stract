00b2cffc502b9fe72f2e701cf36aef52512780aa
Roberto Cedeno <98601749+Vannent@users.noreply.github.com>
2022-10-11 19:04:48 -0700

    fix: update gitignre

--- app/stract_api.py
+++ app/stract_api.py
@@ -1,18 +1,35 @@
 from typing import Union
 from stract import generate_brand_content, generate_brand_hashtags
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
-
+MAX_INPUT_LENGTH = 32
 
 @app.get("/generate_content")
 def generate_content_api(prompt: str):
+    validate_input_length(prompt)
     content = generate_brand_content(prompt)
-    return {"snippet": content}
+    return {"content": content, "keywords": []}
 
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+@app.get("/generate_keywords")
+def generate_hashtags_api(prompt: str):
+    validate_input_length(prompt)
+    hashtags = generate_brand_hashtags(prompt)
+    return {"content": None, "hashtags": hashtags}
+
+@app.get("/generate_snippet_and_keywords")
+async def generate_keywords_api(prompt: str):
+    validate_input_length(prompt)
+    content = generate_brand_content(prompt)
+    hashtags = generate_brand_hashtags(prompt)
+    return {"content": content, "hashtags": hashtags}
+
+def validate_input_length(prompt: str):
+    if len(prompt) >= MAX_INPUT_LENGTH:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Input length is too long. Must be under {MAX_INPUT_LENGTH} characters.",
+        )
 
 # Command: /bin/python3 -m uvicorn stract_api:app --reload
