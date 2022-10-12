fe5dcba6c085a23aa0dfbbec06da50c5d0740e57
Roberto Cedeno <98601749+Vannent@users.noreply.github.com>
2022-10-12 15:06:18 -0700

    feat: add icon svg comp

---
+++ web/components/icons/BackIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const BackIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
+      clipRule="evenodd"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="#4F9C59"
+    >
+      <path d="M13.789 7.155A.75.75 0 0115 7.747v8.501a.75.75 0 01-1.212.591L8.29 12.581a.746.746 0 01.001-1.183l5.498-4.243z"></path>
+    </svg>
+  );
+};
+
+export default BackIcon;
---
+++ web/components/icons/PlayIcon.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const PlayIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fillRule="evenodd"
+      strokeLinejoin="round"
+      strokeMiterlimit="2"
+      clipRule="evenodd"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="#F9A66C"
+    >
+      <path d="M10.211 7.155A.75.75 0 009 7.747v8.501a.75.75 0 001.212.591l5.498-4.258a.746.746 0 00-.001-1.183l-5.498-4.243z"></path>
+    </svg>
+  );
+};
+
+export default PlayIcon;
