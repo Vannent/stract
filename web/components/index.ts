e1ec04bb609af5572f537b6f52a3bc80597f6e1d
Roberto Cedeno <98601749+Vannent@users.noreply.github.com>
2022-10-12 15:06:29 -0700

    feat: add default exp

---
+++ web/components/index.ts
@@ -0,0 +1,4 @@
+export { default as AgainIcon } from "./icons/AgainIcon";
+export { default as BackIcon } from "./icons/BackIcon";
+export { default as GithubIcon } from "./icons/GithubIcon";
+export { default as PlayIcon } from "./icons/PlayIcon";
